efd9a0c19035ede25ff67bea73d4d0d870654959
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2021-10-07 10:00:38 +0100

    Dog years project

---
+++ Dog years.js
@@ -0,0 +1,15 @@
+// My current age
+const myAge = 29;
+// First 2 years of a dogs life
+let earlyYears = 2;
+earlyYears *= 10.5; 
+// substract 2 from current age
+let laterYears = myAge - 2; 
+// Later years of dogs life
+laterYears *= 4;
+// Calculating my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+// write my name in all lowercase
+let myName = 'Augustine'.toLowerCase();
+/* Log the results to the console */
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
